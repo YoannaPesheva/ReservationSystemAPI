90c5ac5b9ca4a63d1c4ebf13a8b8fc2c53e2f0b8
YoannaPesheva <yoannapesheva6802@gmail.com>
2026-02-06 22:26:13 +0200

    add login, password hashing and exceptions

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,6 @@
 # to do: registration and login
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm import Session
 
 import database, models, schemas
@@ -12,11 +13,10 @@ router = APIRouter(
 
 # endpoints
 @router.post("/register", response_model = schemas.UserResponse)
-def register_user(user: schemas.UserCreate, db:Session = Depends(get_db)): # !!!!!
+def register_user(user: schemas.UserCreate, db:Session = Depends(get_db)) -> models.User: # !!!!!
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if db_user:
-        # raise some exception
-        pass
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered!")
     
     hashed_pass = get_hashed_pass(user.password)
     new_user = models.User(email = user.email, hashed_password = hashed_pass, role = user.role)
@@ -25,11 +25,22 @@ def register_user(user: schemas.UserCreate, db:Session = Depends(get_db)): # !!!
     db.refresh(new_user)
     return new_user
 
-@router.post("/login")
-# to do
+@router.post("/login", response_model = schemas.UserResponse)
+def login_user(user: schemas.LoginData, db:Session = Depends(get_db)) -> models.User:
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No user with this email!")
 
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password!")
+    
+    return db_user
 
-def get_hashed_pass(password):
-    # there was a simple algorithm for hashing
-    pass
+def get_hashed_pass(password: str) -> str:
+    salt = gensalt()
+    hashed = hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
